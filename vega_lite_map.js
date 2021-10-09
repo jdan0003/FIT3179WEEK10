09c75a2b755ca54804e73bba608f0dba70d3547e
Mish <89115340+jdan0003@users.noreply.github.com>
2021-10-09 22:37:14 +1100

    Add files via upload

--- vega_lite_map.js
+++ vega_lite_map.js
@@ -1,9 +1,18 @@
 var vg_1 = "currentwork.vg.json";
 var vg_2 = "combinedstacks.vg.json";
 var vg_3 = "worldmapmichelin.vg.json";
+var vg_4 = "zoomedEurope.vg.json";
+var vg_5 = "zoomedEuropedotmap.vg.json";
+var vg_6 = "choroplethmapprice.vg.json";
 
 vegaEmbed("#worldmap2018-2019", vg_1).then(function(result) {}).catch(console.error);
 
 vegaEmbed("#combinedstacks", vg_2).then(function(result){}).catch(console.error);
 
 vegaEmbed("#worldmapmichelin", vg_3).then(function(result){}).catch(console.error);
+
+vegaEmbed("#zoomedEuropeproportional",vg_4).then(function(result){}).catch(console.error);
+
+vegaEmbed("#zoomedEuropedotmap",vg_5).then(function(result){}).catch(console.error);
+
+vegaEmbed("#choroplethmap",vg_6).then(function(result){}).catch(console.error);
